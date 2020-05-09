10c6696822cea83e87a046d02f4c29c205e3807a
Bryan Buschmann <bryan@torchdragon.com>
2020-05-09 16:50:30 -0400

    Add simple Activity.

---
+++ app/src/main/java/com/torchdragon/gish/GishActivity.kt
@@ -0,0 +1,17 @@
+package com.torchdragon.gish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.torchdragon.gish.ui.repositories.RepositoriesFragment
+
+class GishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.gish_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, RepositoriesFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
